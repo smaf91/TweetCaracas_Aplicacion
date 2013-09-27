5649f5fd19fdff61355b232a0bad4b00b44035c9
smaf91 <smaf91@gmail.com>
2013-09-26 19:33:47 -0430

    Agregado logo de la aplicacion. (EL nuevo .apk creado NO va a mostrar los tweets.. ya que trabaja con el servidor pero desde la red de mi casa)

--- platforms/android/gen/R.java.d
+++ platforms/android/gen/R.java.d
@@ -1,5 +1,6 @@
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/gen/com/example/proy/R.java \
- : /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-xhdpi/ic_launcher.png \
+ : /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-xhdpi/icon96.png \
+/home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-xhdpi/ic_launcher.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-xhdpi/icon.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/layout/main.xml \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/xml/config.xml \
@@ -7,8 +8,10 @@
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-hdpi/ic_launcher.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-hdpi/icon.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-mdpi/ic_launcher.png \
+/home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-mdpi/icon48.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-mdpi/icon.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-ldpi/ic_launcher.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-ldpi/icon.png \
+/home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/drawable-ldpi/icon36.png \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/res/values/strings.xml \
 /home/smaf/Documents/USB/proyecto_interfaces/tweetcaracas/platforms/android/bin/AndroidManifest.xml \
