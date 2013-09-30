50f9e809079774917e86ab5ea2f898ed98a7a20b
Sofia Isabel <sofiabravo103@gmail.com>
2013-09-30 01:15:03 -0430

    el slider funciona cargando las columnas por ajax, falta que hayan tweets a dentro de las columnas

--- platforms/android/assets/www/js/cargar_tweets.js
+++ platforms/android/assets/www/js/cargar_tweets.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
 
     //   Cambiar el URL segun la IP en donde este corriendo el servidor.
-    var data_file = "http://10.0.1.87:8000/offi91/columnas/timeline/"
-    //   var data_file = "http://127.0.0.1:8000/Offi91/columnas/timeline/"
+    // var data_file = "http://10.0.1.87:8000/offi91/columnas/timeline/"
+    var data_file = "http://127.0.0.1:8000/Offi91/columnas/timeline/"
     var http_request = new XMLHttpRequest();
    
     
     http_request.onreadystatechange  = function(){
 	if (http_request.readyState == 4  )
 	{	    
-
 	    // Javascript function JSON.parse to parse JSON data
-	    
 	    // alert(http_request.responseText);
 	    var result = JSON.parse(http_request.responseText);
 
