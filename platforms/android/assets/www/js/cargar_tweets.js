054d5efcbf4c7f3b80a8c8f07471077b1c60753e
Sofia Isabel <sofiabravo103@gmail.com>
2013-09-30 03:05:21 -0430

    Funciona el slider cargando las columnas del servidor y rellenandolas con tweets del servidor. Se ven feas las demas columnas aparte de timeline (issue #26)

--- platforms/android/assets/www/js/cargar_tweets.js
+++ platforms/android/assets/www/js/cargar_tweets.js
@@ -1,66 +1,60 @@
-$(document).ready(function(){
+function cargar_tweets(columna){
 
-    //   Cambiar el URL segun la IP en donde este corriendo el servidor.
-    // var data_file = "http://10.0.1.87:8000/offi91/columnas/timeline/"
-    var data_file = "http://127.0.0.1:8000/Offi91/columnas/timeline/"
+    var data_file = "http://127.0.0.1:8000/Offi91/columnas/" + columna + "/"
     var http_request = new XMLHttpRequest();
-   
-    
-    http_request.onreadystatechange  = function(){
+
+    http_request.onreadystatechange = function(){
+
 	if (http_request.readyState == 4  )
 	{	    
-	    // Javascript function JSON.parse to parse JSON data
-	    // alert(http_request.responseText);
+
 	    var result = JSON.parse(http_request.responseText);
 
 	    // result variable now contains the data structure and can
 	    // be accessed as result.name and result.country.
-	   
-	    for (var i = 0, len=result.length; i<len; i++) {
-		var items = []
-		var color = ""
-		items.push("<div style='margin: 5px; border: 1px solid rgba(107, 104, 104, 0.31); border-radius: 4px'>");
-		items.push("<table class ='table table-bordered' style='margin: 0px; border: 1px solid "+result[i]['color']+";"); 
-		items.push("background:"+result[i]['color']+";'>");
-		items.push("<tr>");
-		items.push("<td rowspan='2' style='width: 40px; height: 40px; padding: 0px; border: "+result[i]['color']+";'>");
-		items.push("<a href='perfil.html'>");
-		items.push("<img style='border-radius: 4px' src='"+result[i]['image']+"'/>");
-		items.push("</a>");
-		items.push("</td>");
-		items.push("<td style='padding: 0px; border: "+ result[i]['color'] +";'><b>" + result[i]['name'] + "</b>");
-		items.push(" @" + result[i]['screen_name'] + "</td>");
-		items.push("<td style='border-color:"+result[i]['color']+";'>");
-		items.push("<p style='text-align: right; margin-bottom: 0px; padding: 0px'>7pm</p></td>");
-		items.push("<td style='padding: 3px; border-color:" + result[i]['color'] + " ;'>");
-		items.push("<center>");
-		items.push("<div class='btn-group'>");
-		items.push("<button class='btn' style='padding: 2px'>");
-		items.push("<i class='icon-share-alt'></i>");
-		items.push("</button>");
-		items.push("<button class='btn' style='padding: 2px'>");
-		items.push("<i class='icon-retweet'></i>");
-		items.push("</button>");
-		items.push("<button class='btn' style='padding: 2px'>");
-		items.push("<i class='icon-star'></i>");
-		items.push("</button>");
-		items.push("<button class='btn' style='padding: 2px'>");
-		items.push("<i class='icon-share'></i>");
-		items.push("</button>");
-		items.push("</div>");
-		items.push("</center>");
-		items.push("</td>");
-		items.push("</tr>");
-		items.push("<td colspan='3' style='padding: 0px; border-color: "+result[i]['color']+";'>");
-		items.push(" " + result[i]['text'] + " ");
-		items.push("</td> </table> </div>");
-		$( "<ul/>", {
-		    "class":"my-new-list",
-		    "style": "margin: 0px 0px 0px -20px",
-		    html: items.join("")
-		}).appendTo("#tweets");
-		
 
+	    for (var i = 0, len=result.length; i<len; i++) {
+		var items = ''
+		var color = ""
+		items = items + "<div style='margin: 5px; border: 1px solid rgba(107, 104, 104, 0.31); border-radius: 4px'>";
+		items = items + "<table class ='table table-bordered' style='margin: 0px; border: 1px solid "+result[i]['color']+";"; 
+		items = items + "background:"+result[i]['color']+";'>";
+		items = items + "<tr>";
+		items = items + "<td rowspan='2' style='width: 40px; height: 40px; padding: 0px; border: "+result[i]['color']+";'>";
+		items = items + "<a href='perfil.html'>";
+		items = items + "<img style='border-radius: 4px' src='"+result[i]['image']+"'/>";
+		items = items + "</a>";
+		items = items + "</td>";
+		items = items + "<td style='padding: 0px; border: "+ result[i]['color'] +";'><b>" + result[i]['name'] + "</b>";
+		items = items + " @" + result[i]['screen_name'] + "</td>";
+		items = items + "<td style='border-color:"+result[i]['color']+";'>";
+		items = items + "<p style='text-align: right; margin-bottom: 0px; padding: 0px'>7pm</p></td>";
+		items = items + "<td style='padding: 3px; border-color:" + result[i]['color'] + " ;'>";
+		items = items + "<center>";
+		items = items + "<div class='btn-group'>";
+		items = items + "<button class='btn' style='padding: 2px'>";
+		items = items + "<i class='icon-share-alt'></i>";
+		items = items + "</button>";
+		items = items + "<button class='btn' style='padding: 2px'>";
+		items = items + "<i class='icon-retweet'></i>";
+		items = items + "</button>";
+		items = items + "<button class='btn' style='padding: 2px'>";
+		items = items + "<i class='icon-star'></i>";
+		items = items + "</button>";
+		items = items + "<button class='btn' style='padding: 2px'>";
+		items = items + "<i class='icon-share'></i>";
+		items = items + "</button>";
+		items = items + "</div>";
+		items = items + "</center>";
+		items = items + "</td>";
+		items = items + "</tr>";
+		items = items + "<td colspan='3' style='padding: 0px; border-color: "+result[i]['color']+";'>";
+		items = items + " " + result[i]['text'] + " ";
+		items = items + "</td> </table> </div>";		
+
+		var target = "#" + columna;
+
+		$(items).appendTo(target);
 
 		// Formato al slider
 		$(".bx-viewport").css("height","auto");
@@ -68,11 +62,9 @@ $(document).ready(function(){
 	    }
 	}
     }
-    
-    http_request.open("GET", data_file, true);
-    http_request.setRequestHeader('Authorization', "Basic " + Base64.encode("offi91" + ':' + "12345"));
-//     http_request.setRequestHeader('Authorization', "Basic " + Base64.encode("smaf91" + ':' + "12345"));
-    http_request.send();
-    
-});
 
+    http_request.open("GET", data_file, false);
+    http_request.setRequestHeader('Authorization', "Basic " + Base64.encode("offi91" + ':' + "12345"));
+    http_request.send();
+
+}
