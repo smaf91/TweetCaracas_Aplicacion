dd9e2d82d4accf5364fcae651598c62e61fac93a
smaf91 <smaf91@gmail.com>
2013-09-29 23:55:29 -0430

    Agregado request para hacer login.

--- platforms/android/assets/www/js/cargar_listas_activas.js
+++ platforms/android/assets/www/js/cargar_listas_activas.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
       items.push("<ul class='nav nav-pills nav-stacked'>");
       items.push("<li>");
       items.push("<a href='#' style='color: #848484; border-radius: 0; padding: 15px; margin: 0; border-top: 0.5px solid rgba(116, 116, 116, 0.20);border-bottom: 1px solid rgba(116, 116, 116, 0.20);'>");
-      items.push("<button class='btn btn-danger' style='margin-right: 10px; padding: 5px; padding-bottom: 0px; padding-top: 0px'> x </button>");
+      items.push("<button id='borrar' class='btn btn-danger' style='margin-right: 10px; padding: 5px; padding-bottom: 0px; padding-top: 0px'> x </button>");
       items.push(result[i]['nombre']);
       items.push("<p style='float:right; margin: 8px;'> 10 members </p>");
       items.push("</a>");
@@ -28,6 +28,6 @@ $(document).ready(function(){
       }
    }
   http_request.open("GET", data_file, true);
-  http_request.setRequestHeader('Authorization', "Basic " + Base64.encode("offi91" + ':' + "12345"));
+//   http_request.setRequestHeader('Authorization', "Basic " + Base64.encode("offi91" + ':' + "12345"));
   http_request.send();
 });
