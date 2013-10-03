f8cc1a32839f49ec32852537cd82349326bc3376
smaf91 <smaf91@gmail.com>
2013-10-03 17:43:42 -0430

    Arreglado estilo de las columnas

--- platforms/android/assets/www/js/cargar_tweets.js
+++ platforms/android/assets/www/js/cargar_tweets.js
@@ -3,8 +3,8 @@ function cargar_tweets(columna){
     var usuario = localStorage['usuario_autenticado'];
 
     //   Cambiar el URL segun la IP en donde este corriendo el servidor.
-    var data_file = "http://10.0.1.87:8000/"+usuario+"/columnas/"+columna+"/"
-    //   var data_file = "http://127.0.0.1:8000/Offi91/columnas/timeline/"
+//     var data_file = "http://10.0.1.87:8000/"+usuario+"/columnas/"+columna+"/"
+    var data_file = "http://0.0.0.0:8000/"+usuario+"/columnas/"+columna+"/"
     var http_request = new XMLHttpRequest();
 
     http_request.onreadystatechange = function(){
@@ -20,7 +20,7 @@ function cargar_tweets(columna){
 	    for (var i = 0, len=result.length; i<len; i++) {
 		var items = ''
 		var color = ""
-		items = items + "<div style='margin: 5px; border: 1px solid rgba(107, 104, 104, 0.31); border-radius: 4px'>";
+		items = items + "<div style='margin: 5px; border: 1px solid rgba(107, 104, 104, 0.31); border-radius: 4px; width: 93%;'>";
 		items = items + "<table class ='table table-bordered' style='margin: 0px; border: 1px solid "+result[i]['color']+";"; 
 		items = items + "background:"+result[i]['color']+";'>";
 		items = items + "<tr>";
@@ -32,7 +32,7 @@ function cargar_tweets(columna){
 		items = items + "<td style='padding: 0px; border: "+ result[i]['color'] +";'><b>" + result[i]['name'] + "</b>";
 		items = items + " @" + result[i]['screen_name'] + "</td>";
 		items = items + "<td style='border-color:"+result[i]['color']+";'>";
-		items = items + "<p style='text-align: right; margin-bottom: 0px; padding: 0px'>7pm</p></td>";
+		items = items + "<p style='text-align: right; margin-bottom: 0px; padding: 0px'>"+result[i]['created_at']+"</p></td>";
 		items = items + "<td style='padding: 3px; border-color:" + result[i]['color'] + " ;'>";
 		items = items + "<center>";
 		items = items + "<div class='btn-group'>";
