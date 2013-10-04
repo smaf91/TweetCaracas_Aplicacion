18cd0ba2b5e3347756474b461d839cf10e046c2b
smaf91 <smaf91@gmail.com>
2013-10-04 16:15:17 -0430

    Arreglada personalizar_columna, twittea y manda dm en TODAS las vistas

--- platforms/android/assets/www/js/twittear.js
+++ platforms/android/assets/www/js/twittear.js
@@ -1,10 +1,8 @@
 function twittear(){
-  alert(localStorage['usuario_autenticado']);
   var form = $("#formT").serializeArray()[0]['value'];
   var data_file = "http://0.0.0.0:8000/"+localStorage['usuario_autenticado']+"/tweet/text=\""+form+"\"/";
   var http_request = new XMLHttpRequest();
   
-  alert(data_file);
   http_request.open("POST", data_file, false);
   http_request.send();
 }
